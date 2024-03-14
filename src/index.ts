d08fa7941b097595d5278bac4176ab02c59d918f
shtse8 <shtse8@gmail.com>
2024-03-15 04:23:14 +0800

    Add text module to index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export * from './fn-ops'
 export * from './str'
 export * from './misc'
 export * from './array'
+export * from './text'
 
 import * as typed from './typed'
 import * as types from './types'
@@ -12,11 +13,13 @@ import * as fnOps from './fn-ops'
 import * as str from './str'
 import * as misc from './misc'
 import * as array from './array'
+import * as text from './text'
 export default {
     ...typed,
     ...types,
     ...fnOps,
     ...str,
     ...misc,
-    ...array
+    ...array,
+    ...text
 }
